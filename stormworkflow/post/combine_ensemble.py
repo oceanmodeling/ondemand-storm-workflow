345a86ff44506f29140fc0019be17b18f105d021
Fariborz Daneshvar <fariborz.daneshvar@noaa.gov>
2024-07-02 17:02:11 +0000

    update combine_ensemble.py to only combine elevations

--- stormworkflow/post/combine_ensemble.py
+++ stormworkflow/post/combine_ensemble.py
@@ -15,6 +15,7 @@ def main(args):
     output = combine_results(
         model='schism',
         adcirc_like=True,
+        filenames=['out2d_*.nc'], #only combine elevations. 
         output=ensemble_dir / 'analyze',
         directory=ensemble_dir,
         parallel=not args.sequential,
